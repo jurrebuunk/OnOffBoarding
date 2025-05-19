7b7e8d3fba3a13e31c2c96cf144227dab9f91167
Jurre Buunk <jurre@buunk.org>
2025-05-19 14:02:13 +0200

    test

--- adcreateuser.py
+++ adcreateuser.py
@@ -6,21 +6,19 @@ ldap_server = 'ldap://192.168.2.14'
 ldap_user = "BUUNK\\ldap-search"
 ldap_password = os.getenv('AD_SEARCH_PW')
 
-print(ldap_user)
-print(ldap_password)
-
 # New user details from environment variables
-new_username = os.getenv('username')
+cn = os.getenv('cn')
+given_name = os.getenv('givenName')
+surname = os.getenv('sn')
+initials = os.getenv('initials') or ''
+name = os.getenv('name')
+user_principal_name = os.getenv('userPrincipalName')
+sam_account_name = os.getenv('sAMAccountName')
+email = os.getenv('mail')
+telephone = os.getenv('telephoneNumber') or ''
 new_password = os.getenv('password')
-first_name = os.getenv('firstname')
-last_name = os.getenv('lastname')
 
-user_dn = f'cn={new_username},ou=Domain-Users,dc=buunk,dc=org'
-
-print("username:", new_username)
-print("firstname:", first_name)
-print("lastname:", last_name)
-print("password:", new_password)
+user_dn = f'cn={cn},ou=Domain-Users,dc=buunk,dc=org'
 
 # Connect to the server
 server = Server(ldap_server, get_info=ALL)
@@ -28,12 +26,16 @@ conn = Connection(server, user=ldap_user, password=ldap_password, authentication
 
 # Add the new user
 conn.add(user_dn, ['top', 'person', 'organizationalPerson', 'user'], {
-    'cn': new_username,
-    'givenName': first_name,
-    'sn': last_name,
-    'displayName': f'{first_name} {last_name}',
-    'sAMAccountName': new_username,
-    'userPrincipalName': f'{new_username}@buunk.org'
+    'cn': cn,
+    'givenName': given_name,
+    'sn': surname,
+    'initials': initials,
+    'displayName': name,
+    'name': name,
+    'userPrincipalName': user_principal_name,
+    'sAMAccountName': sam_account_name,
+    'mail': email,
+    'telephoneNumber': telephone
 })
 
 # Set the user's password
@@ -44,5 +46,4 @@ conn.modify(user_dn, {
     'userAccountControl': [(MODIFY_REPLACE, [512])]
 })
 
-# Unbind the connection
 conn.unbind()
